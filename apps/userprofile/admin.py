68fb9469c616e5963d40ea1731a73f9fe9c0e3db
pinsily <13160724868@163.com>
2022-01-02 23:30:41 +0800

    add:admin

--- apps/userprofile/admin.py
+++ apps/userprofile/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
+from userprofile.models import Profile
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        "index",
+        "username",
+        "email",
+        "nickname",
+        "status",
+        "version",
+        "create_time",
+        "modify_time",
+    )
+
+
+admin.site.register(Profile, ProfileAdmin)
